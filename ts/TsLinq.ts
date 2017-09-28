96a4bed6c9239a8769eb7e72e72a3eb56c2abce7
aPisC <cser.bende@gmail.com>
2017-09-28 11:34:33 +0200

    asd

--- ts/TsLinq.ts
+++ ts/TsLinq.ts
@@ -11,8 +11,8 @@ export class TsLinqHandler {
       this.arr.forEach((e, i) => { if (exp(e, i)) b.push(e); });
       return new TsLinqHandler(b);
    }
-   public Count( exp : any = undefined): number {
-      if (exp === undefined) this.arr.length;
+   public Count( exp : Function = undefined): number {
+      if (exp === undefined) return this.arr.length;
       let b: number = 0;
       this.arr.forEach((e, i) => { if (exp(e, i)) b++; });
       return b;
--- ts/content.ts
+++ ts/content.ts
@@ -14,14 +14,14 @@ export class Content {
         Kód.ResetSorszam();
         fs.readFileSync("ajto.txt").toString().split("\n")
             .forEach(x => k.push(new Kód(x.trim())));
-        res.write("2. feladat: Kérem a zár kódszámát: ");
+        res.write("<br />2. feladat: Kérem a zár kódszámát: ");
         const zárKód: string = "239451";
-        res.write("3. feladat: A nyitó kódszámok sorai: "
+        res.write("<br />3. feladat: A nyitó kódszámok sorai: "
             + new TsLinqHandler(k).Where(x => x.K === zárKód).Select(x => x.Ssz).ToArray().join(" "));
         const ism: any = new TsLinqHandler(k).Where(x => x.IsmétlésVan);
         res.write((ism.Count() === 0 ? ("4. feladat: nem volt ismétlődő számjegy") :
-            ("4. feladat: Az első ismétlődést tartalmazó próbálkozás sorszáma: " + ism.First().Ssz)));
-        res.write( "5. feladat: Egy " + zárKód.length.toString() +" hosszú kódszám: " + Kód.Generál(zárKód.length));
+            ("<br />4. feladat: Az első ismétlődést tartalmazó próbálkozás sorszáma: " + ism.First().Ssz)));
+        res.write( "<br />5. feladat: Egy " + zárKód.length.toString() +" hosszú kódszám: " + Kód.Generál(zárKód.length));
         //fs.writeFileSync("siker.txt", new TsLinqHandler(k).Select(x=> x.k + x.Minősít(zárKód) ).ToArray().join("\n") );
         res.end();
     }
