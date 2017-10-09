659ad6eb9366d1c7c073394a099fb85768c30208
Ahega0 <31726601+Ahega0@users.noreply.github.com>
2017-10-09 14:08:10 +0200

    "tiszta kód"

--- ts/TsLinq.ts
+++ ts/TsLinq.ts
@@ -6,31 +6,31 @@ export class TsLinqHandler {
    public ToArray(): any[] {
       return this.arr;
    }
-   public Where( exp: Function): TsLinqHandler {
+   public Where(exp: Function): TsLinqHandler {
       const b: any[] = [];
       this.arr.forEach((e, i) => { if (exp(e, i)) b.push(e); });
       return new TsLinqHandler(b);
    }
-   public Count( exp : Function = undefined): number {
+   public Count(exp: Function = undefined): number {
       if (exp === undefined) return this.arr.length;
       let b: number = 0;
       this.arr.forEach((e, i) => { if (exp(e, i)) b++; });
       return b;
    }
-   public Sum( exp: Function): number {
+   public Sum(exp: Function): number {
       if (exp === undefined) exp = (e) => e;
       let b: number = 0;
       this.arr.forEach((e, i) => { b += exp(e, i); });
       return b;
    }
-   public Select( exp: Function): TsLinqHandler {
+   public Select(exp: Function): TsLinqHandler {
       if (exp === undefined) return this;
       const b: any[] = [];
       this.arr.forEach((e, i) => { b.push(exp(e, i)); });
       return new TsLinqHandler(b);
    }
    public First(): any {
-      if (this.arr.length == 0) return undefined;
+      if (this.arr.length === 0) return undefined;
       return this.arr[0];
    }
    public Join(separator: string): string {
