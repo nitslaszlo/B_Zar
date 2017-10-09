f259da8da62af22216411e254c574938fcc334f4
Ahega0 <31726601+Ahega0@users.noreply.github.com>
2017-10-09 13:57:43 +0200

    "tiszta kód"

--- ts/content.ts
+++ ts/content.ts
@@ -16,7 +16,6 @@ export class Content {
             .forEach(x => k.push(new Kód(x.trim())));
 
         res.write("<br />2. feladat: Kérem a zár kódszámát: ");
-        //const zárKód: string = "239451";
         const zárKód: string | undefined = url.parse(req.url, true).query["zarkod"];
         if (zárKód === undefined) {
             res.write("<form method='GET'><input type='text' name='zarkod'/></form>");
@@ -27,14 +26,14 @@ export class Content {
 
         res.write("<br />3. feladat: A nyitó kódszámok sorai: " + new TsLinqHandler(k).Where(x => x.K === zárKód)
             .Select(x => x.Ssz).ToArray().join(" "));
-            + new TsLinqHandler(k)
+
         const ism: any = new TsLinqHandler(k).Where(x => x.IsmétlésVan);
 
         res.write((ism.Count() === 0 ? ("4. feladat: nem volt ismétlődő számjegy") :
             ("<br />4. feladat: Az első ismétlődést tartalmazó próbálkozás sorszáma: " + ism.First().Ssz)));
 
         res.write( "<br />5. feladat: Egy " + zárKód.length.toString() +" hosszú kódszám: " + Kód.Generál(zárKód.length));
-        //fs.writeFileSync("siker.txt", new TsLinqHandler(k).Select(x=> x.k + x.Minősít(zárKód) ).ToArray().join("\n") );
+        fs.writeFileSync("siker.txt", new TsLinqHandler(k).Select(x=> x.k + x.Minősít(zárKód)).ToArray().join("\n"));
 
         res.end();
     }
